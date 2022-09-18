f37453acc72a7b8a66cc4766a9004e91d80f144b
josieyan123 <josieyan123@gmail.com>
2022-09-18 13:54:59 +0800

    finish and pass all tests in CollectionOperator

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,29 +1,41 @@
 package com.thoughtworks.collection;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new UnsupportedOperationException();
+        if(left < right){
+            return IntStream.rangeClosed(left, right).boxed().collect(Collectors.toList());
+        }
+        return IntStream.rangeClosed(right, left).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new UnsupportedOperationException();
+        return getListByInterval(left, right).stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElements(int[] array) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(array).filter(i -> i % 2 == 0).boxed().collect(Collectors.toList());
     }
 
     public int popLastElement(int[] array) {
-        throw new UnsupportedOperationException();
+        return array[array.length - 1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new UnsupportedOperationException();
+        List<Integer> firstList = Arrays.stream(firstArray).boxed().collect(Collectors.toList());
+        List<Integer> secondList = Arrays.stream(secondArray).boxed().collect(Collectors.toList());
+        firstList.retainAll(secondList);
+        return firstList;
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new UnsupportedOperationException();
+        List<Integer> firstList = Arrays.stream(firstArray).collect(Collectors.toList());
+        List<Integer> secondList = Arrays.stream(secondArray).collect(Collectors.toList());
+        firstList.addAll(secondList);
+        return firstList.stream().distinct().collect(Collectors.toList());
+
     }
 }
