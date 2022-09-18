93fafc11c24c0dd117ee6d69df1b74b300438fb3
josieyan123 <josieyan123@gmail.com>
2022-09-18 22:14:56 +0800

    finish and pass all tests in MyMap

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -1,25 +1,28 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
     List<Integer> array;
-    private String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    private final String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-    private List<String> letterList = Arrays.asList(letters);
+    private final List<String> letterList = Arrays.asList(letters);
 
     public MyMap(List<Integer> array) {
         this.array = array;
     }
 
     public List<Integer> getTriple() {
-        throw new UnsupportedOperationException();
+        return array.stream().map(i -> i * 3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new UnsupportedOperationException();
+        return array.stream().map(i -> letterList.get(i - 1)).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
@@ -27,10 +30,10 @@ public class MyMap {
     }
 
     public List<Integer> sortFromBig() {
-        throw new UnsupportedOperationException();
+        return array.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new UnsupportedOperationException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
 }
